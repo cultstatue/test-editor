64c4b9ebac3cce5f5cc5539a78821629b020e4f6
cultstatue <saltedchurros@gmail.com>
2022-10-02 18:05:42 -0400

    added put and get routes and fixed an error with editor.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,19 +1,57 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
 
-export const initDb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  // We are creating a new database named 'contact_db' which will be using version 1 of the database.
+  openDB('jate_db', 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('content')) {
+        console.log('contents store already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+      // Create a new object store for the data and give it a key name of 'id' which will increment automatically
+      db.createObjectStore('content', { keyPath: 'id', autoIncrement: true });
+      console.log('contents store created');
+    }
+  })
+}
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  
+  const contactDb = await openDB('jate_db', 1);
+
+  const tx = contactDb.transaction('content', 'readwrite');
+
+  const store = tx.objectStore('content');
+
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const contactDb = await openDB('jate_db', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction('content', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('content');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+initdb();
